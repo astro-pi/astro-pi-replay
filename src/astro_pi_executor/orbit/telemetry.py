4f70b4d00c6d779a4146eb9fe4ca09da818f124d
Geraint Ballinger <G3zz@users.noreply.github.com>
2023-08-25 12:18:45 +0100

    More logs

--- src/astro_pi_executor/orbit/telemetry.py
+++ src/astro_pi_executor/orbit/telemetry.py
@@ -27,6 +27,7 @@ def coordinates(satellite: skyfield.api.EarthSatellite) -> GeographicPosition:
     now: Time = _timescale.now()
     logger.debug(f"original now: {now}")
     rel_pos: Geocentric = typing.cast(Geocentric, satellite.at(now))
+    logger.debug(f"rel_pos.t: {rel_pos.t}")
     return rel_pos.subpoint()
 
 
