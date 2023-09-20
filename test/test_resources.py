74d723cab7e79b61f753f27aff39f5c780c4c257
Geraint Ballinger <G3zz@users.noreply.github.com>
2023-09-20 17:58:47 +0100

    Add resource test

---
+++ test/test_resources.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.skip(reason="TODO")
+def test_each_replay_dir_with_tests():
+    # e.g. need to ensure that AstroX works with sense_hat
+    # and AstroX
+    pass
+
+# also check that the order of precedence is being respected
