b46ed04c45d9c6718b78eddc5b928e10ebcff303
Geraint Ballinger <G3zz@users.noreply.github.com>
2024-02-05 16:43:52 +0000

    Add local wheel option to Windows smoke test

--- test/smoke_tests/execute_smoke_tests.bat
+++ test/smoke_tests/execute_smoke_tests.bat
@@ -1,7 +1,66 @@
 @echo off
 
-rem Set the desired virtual environment name
+rem GLOBALS
+set LOCAL="local"
 set VENV_NAME=venv
+set TRUE=true
+for %%I in ("%~dp0\..") do set PROJECT_ROOT=%%~dpI
+for %%I in (%0) do set SCRIPT_NAME=%%~nxI
+
+goto :main
+
+rem HELPER FUNCTIONS
+:bold
+  setlocal enabledelayedexpansion
+  echo [1m %~1 [0m
+  endlocal
+exit /b
+
+:usage
+call :bold "NAME"
+echo     %SCRIPT_NAME%
+call :bold "SYNOPSIS"
+echo     %SCRIPT_NAME% [--%LOCAL% ^| -h]
+call :bold "DESCRIPTION"
+echo     Execute the smoke tests in this directory using the latest installable
+echo     wheel from TestPyPI. The test simply consists of taking a picture using
+echo     the PiCamera API.
+echo.
+echo     The following options are available:
+echo.
+echo         --%LOCAL% Use a local wheel instead of the one from TestPyPI.
+exit /b
+
+:parse_args
+  setlocal enabledelayedexpansion
+  if "%1"=="" (
+    exit /b
+  ) else if "%1"=="--local" (
+    set USE_LOCAL_WHEEL="%TRUE%"
+  ) else if "%1"=="-h" (
+    call :usage
+    exit /b
+  ) else (
+    call :usage
+    exit /b 1
+  )
+  endlocal & set "USE_LOCAL_WHEEL=%USE_LOCAL_WHEEL%"
+exit /b
+
+:mktempd
+  setlocal enabledelayedexpansion
+  set "temp_template=%TEMP%\TempDir_"
+  set "tempdir=!temp_template!!random!"
+  if exist "!tempdir!" goto mktempd
+  mkdir "!tempdir!"
+  endlocal & set "tempdir=%tempdir%"
+exit /b
+
+rem MAIN SCRIPT START ####
+:main
+
+rem Parse the first arg
+call :parse_args %1
 
 rem Create the virtual environment
 python -m venv %VENV_NAME%
@@ -9,8 +68,25 @@ python -m venv %VENV_NAME%
 rem Activate the virtual environment
 call %VENV_NAME%\Scripts\Activate.bat
 
-rem Install dependencies
-pip install -r requirements.txt
+if "%USE_LOCAL_WHEEL%"=="%TRUE%" (
+  echo Installing local wheel
+  call :mktempd
+  pip install build
+  python -m build --outdir "%tempdir%" "%PROJECT_ROOT%"
+  set wheel_count=0
+  for %%F in ("%tempdir%\*.whl") do (
+    set \a wheel_count+=1
+    set wheel=%%F
+  )
+  if not %wheel_count% equ 1 (
+    echo There was a problem building the wheel
+    exit /b 1
+  )
+  set SMOKE_TEST_LOCAL_WHEEL=%wheel%
+) else (
+  rem Install dependencies
+  pip install -r requirements.txt
+)
 
 rem Provide user instructions
 echo Virtual environment "%VENV_NAME%" has been created and activated.
@@ -22,6 +98,8 @@ set PYTEST_PROFILE=SMOKE_TESTS
 pytest -o log_cli=true --log-cli-level=DEBUG --noconftest
 set PYTEST_ERROR=%ERRORLEVEL%
 echo %PYTEST_ERROR%
+rem Unset environment variables
 set ASTRO_PI_EXECUTOR_REPLAY_SEQUENCE=
 set PYTEST_PROFILE=
+set SMOKE_TEST_LOCAL_WHEEL=
 if %PYTEST_ERROR% GEQ 1 EXIT /B %PYTEST_ERROR%
