979790aa4f3e98c86544b36bef43ebf5f09bb629
Geraint Ballinger <G3zz@users.noreply.github.com>
2024-02-06 12:16:19 +0000

    Bump version number

--- src/astro_pi_replay/__init__.py
+++ src/astro_pi_replay/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-__version__ = "0.0.19"
+__version__ = "0.0.20"
 PROGRAM_CMD_NAME = "Astro-Pi-Replay"
 PROGRAM_NAME = "astro_pi_replay"
 PACKAGE_ROOT = Path(__file__).parent
