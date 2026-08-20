3bb080574c36058334aba9235237dacb2c86df3d
Geraint Ballinger <G3zz@users.noreply.github.com>
2026-08-20 15:07:29 +0100

    feat: add coordinates section to build_metadata

--- src/scripts/asset-preparation-scripts/build_metadata.py
+++ src/scripts/asset-preparation-scripts/build_metadata.py
@@ -1,23 +1,26 @@
-from datetime import datetime, timezone
+from datetime import datetime, time, timedelta, timezone
 from pathlib import Path
 from typing import cast
 import logging
+import numpy as np
 
 from skyfield.positionlib import Geocentric
 
 
 from skyfield.api import Loader
 
-def get_altitude_average_km(
-    start: datetime, 
-    end: datetime,
-    tle_filepath: Path
-) -> float:
-    start = start.replace(tzinfo=timezone.utc)
-    end  = end.replace(tzinfo=timezone.utc)
 
+def _get_iss_coords(
+    start: datetime,
+    sequence_length: timedelta,
+    tle_filepath: Path
+) -> Geocentric:
+    """
+    Returns the ISS positions in the GCRF at each minute from the
+    given sequence start time for the required sequence length.
+    """
+    start = start.replace(tzinfo=timezone.utc)
     logging.debug(f"start: {start}")
-    logging.debug(f"end: {end}")
 
     load = Loader(str(tle_filepath.parent.resolve()))
 
@@ -26,20 +29,52 @@ def get_altitude_average_km(
 
     ts = load.timescale()
 
-    start_elevation: float = cast(
-        float,
-        cast(Geocentric, 
-             iss.at(ts.from_datetime(start))).subpoint().elevation.km)
-    end_elevation: float = cast(
-        float,
-        cast(Geocentric,
-             iss.at(ts.from_datetime(end))).subpoint().elevation.km)
+    dts = []
+    for i in range(round(sequence_length.total_seconds() / 60)):
+        dt = (start + timedelta(minutes=i)).replace(
+                tzinfo=timezone.utc)
+        dts.append(dt)
 
-    logging.debug(f"elevation at {start}: {start_elevation}")
-    logging.debug(f"elevation at {end}: {end_elevation}")
+    dts = ts.from_datetimes(dts)
+    return iss.at(dts)
 
-    return (start_elevation + end_elevation) / 2
 
+def get_iss_altitude_average_km(
+    start: datetime,
+    sequence_length: timedelta,
+    tle_filepath: Path
+) -> float:
+    coords =  _get_iss_coords(start, sequence_length, tle_filepath)
+    return np.mean(coords.subpoint().elevation.km)
+
+def get_iss_wgs84_coordinates(
+    start: datetime,
+    sequence_length: timedelta,
+    tle_filepath: Path
+):
+    coords = _get_iss_coords(start, sequence_length, tle_filepath).subpoint()
+
+    lats = np.array(coords.latitude.signed_dms()).T # (N, 4)
+    longs = np.array(coords.longitude.signed_dms()).T # (N, 4)
+    altitudes = coords.elevation.km # (N,)
+
+    coord_dicts = []
+    for i in range(len(altitudes)):
+        coord_dict = {}
+        for key, point in [
+                ("latitude", lats[i,:]), 
+                ("longitude", longs[i,:])
+        ]:
+            s,d,m,sec = point
+            coord_dict[key] = {
+                "sign": "+" if s > 0 else "-",
+                "degrees": d,
+                "minutes": m,
+                "seconds": sec
+            }
+        coord_dict["altitude"] = altitudes[i]
+        coord_dicts.append(coord_dict)
+        return coord_dicts
 
 # if __name__ == "__main__":
 #     parser = argparse.ArgumentParser(
@@ -47,13 +82,13 @@ def get_altitude_average_km(
 #         description="")
 #     parser.add_argument("start", help="The start time in ISO format.")
 #     parser.add_argument("end", help="The start time in ISO format.")
-#     parser.add_argument("tle", type=Path, 
+#     parser.add_argument("tle", type=Path,
 #                         help="The path to the TLE file")
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    alt = get_altitude_average_km(
+    alt = get_iss_altitude_average_km(
         datetime(2023,5,3,3,22,25),
         datetime(2023,5,3,3,32,26),
         Path(__file__).parent / "upsampled-resequenced" \
